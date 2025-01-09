f07086a36b3fdded12dcf64137bfba7656b49601
devicarus <devicarus@icloud.com>
2025-01-09 22:50:03 +0100

    🎨 Fixed indentation (again)

--- index.ts
+++ index.ts
@@ -3,14 +3,14 @@ import PDFDocument from 'pdfkit'
 const imageToPDF = (pages: (string | Buffer)[], size: PDFKit.PDFDocumentOptions['size']): PDFKit.PDFDocument => {
   const doc: PDFKit.PDFDocument = new PDFDocument({ margin: 0, size })
 
-	for (let index: number = 0; index < pages.length; index++) {
-		if (index != 0) doc.addPage()
+  for (let index: number = 0; index < pages.length; index++) {
+    if (index != 0) doc.addPage()
 
-		doc.image(pages[index], 0, 0, { 
-			fit: [doc.page.width - doc.page.margins.left - doc.page.margins.right, doc.page.height - doc.page.margins.top - doc.page.margins.bottom],
-			align: 'center', valign: 'center' 
-		})
-	}
+    doc.image(pages[index], 0, 0, { 
+	    fit: [doc.page.width - doc.page.margins.left - doc.page.margins.right, doc.page.height - doc.page.margins.top - doc.page.margins.bottom],
+      align: 'center', valign: 'center' 
+    })
+  }
 
   doc.end()
 
