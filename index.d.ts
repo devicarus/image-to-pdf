87fddfc0d593a0b37219dbeb9de251a040448d8b
devicarus <devicarus@icloud.com>
2025-01-09 22:31:41 +0100

    :coffin: Removed old types, already using auto-generated ones

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import PDFKit = require('pdfkit');
-
-declare function imageToPDF(
-    pages: (string | Buffer)[],
-    size: PDFKit.PDFDocumentOptions['size'],
-): PDFKit.PDFDocument;
-
-export = imageToPDF;
